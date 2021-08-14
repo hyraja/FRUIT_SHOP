cd925496f2bf10252050d5b19f2b28717d397b69
hyraja <paikaray.milan@gmail.com>
2021-08-14 18:38:05 +0530

    display bill system nd cart function added

--- fruit_shop.py
+++ fruit_shop.py
@@ -1,7 +1,7 @@
 print('Fruit management system')
 
-f = {}
-cart = []
+f = {}  # empty dictionary
+cart = {}  # empty dictionary
 
 # small decorator for displaying data
 
@@ -16,26 +16,22 @@ def my_decorator(func):
     return wrap_func
 
 
+@my_decorator
 def main_menu():
     print("1.Add fruit ")
-    print("2.-Delete fruit by name")
+    print("2.Delete fruit by name")
     print("3.Search fruit by name and rate")
     print("4.change fruit name and rate")
-    print("5.-Add to cart")
-    print("6.-Display all fruit data")
-    print("7.display cart")
-    print('8.Delete item from cart')
-    print('9.exit')
+    print("5.Display all fruit data")
+    print("6.manage cart")
+    print('7.exit')
 
 
 @my_decorator
 def add_fruit():
-    f_id = 0
-    s_no = int(input('Enter serial number '))
+    s_no = int(input('Enter serial number : '))
     if s_no not in f.keys():
-        f_id = f_id + 1
         f[s_no] = {
-            'fruit_id': f_id,
             'fruit_name': input("Enter fruit name : "),
             'Fruit_rate': int(input('Enter rate of fruit : ')),
             'imported_from': input('Enter import place of fruit : '),
@@ -50,23 +46,24 @@ def add_fruit():
 
 @my_decorator
 def delete_fruit():
-    s_no = int(input('enter serial number'))
+    s_no = int(input('enter serial number : '))
     if s_no not in f.keys():
         print('wrong serial number')
     else:
         del f[s_no]
+        print("fruit deleted successfully")
 
 
 @my_decorator
 def search_fruit():
-    print('a,A => search by name \n b,B => search by rate')
+    print(' a,A => search by name \n b,B => search by rate')
     choice = input('enter choice: ')
     if choice == 'a' or choice == 'A':
         for i in f.values():
             name = input('Enter fruit name to search : ')
             if i['fruit_name'] == name:
                 print(
-                    f" {i['fruit_id']} | {i['fruit_name']} |{i['Fruit_rate']} | {i['imported_from']} |{i['imported_date']} | {i['buy_price']}  ")
+                    f" {i['fruit_name']} |{i['Fruit_rate']} | {i['imported_from']} |{i['imported_date']} | {i['buy_price']}  ")
             else:
                 print('wrong fruit name ')
     elif choice == 'b' or choice == 'B':
@@ -74,7 +71,7 @@ def search_fruit():
             rate = float(input('provide rate for searching like xx.xx'))
             if v['fruit_rate'] == rate:
                 print(
-                    f" {v['fruit_id']} | {v['fruit_name']} |{v['Fruit_rate']} | {v['imported_from']} |{v['imported_date']} | {v['buy_price']}  ")
+                    f"{v['fruit_name']} |{v['Fruit_rate']} | {v['imported_from']} |{v['imported_date']} | {v['buy_price']}  ")
             else:
                 print('Not available with this price')
 
@@ -92,17 +89,7 @@ def change_fruit():
 
 
 @my_decorator
-def add_to_cart():
-    for i in f.keys():
-        print(f'fruit ids :  {i} ')
-
-    print('press on fruit id number to add to cart ')
-    cart.append(f[int(input('enter fruit id to add on cart : '))])
-    print('Fruit added in the cart successfully')
-
-
-@my_decorator
-def diplay_fruit():
+def display_fruit():
     if len(f) == 0:
         print('!' * 30)
         print('There is no fruit available')
@@ -113,17 +100,63 @@ def diplay_fruit():
 
 
 @my_decorator
-def display_cart():
-    print(cart)
+def manage_cart_menu():
+    print("\t1.Add item to cart")
+    print("\t2.Delete item from cart")
+    print("\t3.Display cart and bill")
+    print("\t4.Exit")
+
+
+@my_decorator
+def add_to_cart():
+    display_fruit()
+    serial_no = int(input('Enter serial number: '))
+    cart[serial_no] = f[serial_no]
+    print(f'Fruit added in the cart successfully')
 
 
 @my_decorator
 def delete_from_cart():
-    display_cart()
-    print('*'*20)
+    display_cart_and_bill()
+    print('!'*30)
     s_no = int(input(('Enter fruit_id for delete fruit from cart : ')))
-    cart.remove(f[s_no])
+    del cart[s_no]
     print(f'fruit removed from cart !!!!')
+
+
+@my_decorator
+def display_cart_and_bill():
+    if len(cart) == 0:
+        print('!' * 30)
+        print('There is no fruit available')
+    else:
+        sum = 0
+        for i, j in cart.items():
+            print(
+                f" {i} | {j['fruit_name']} |{j['Fruit_rate']} | {j['imported_from']} |{j['imported_date']} | {j['buy_price']}  ")
+            sum += j['buy_price']
+        print('*'*20, f'price : {sum}', '*'*20)
+
+
+@my_decorator
+def manage_cart():
+    while True:
+        manage_cart_menu()
+        ch = int(input("enter choice : "))
+        if ch == 1:
+            # add item to cart
+            add_to_cart()
+
+        elif ch == 2:
+            # delete item from cart
+            delete_from_cart()
+        elif ch == 3:
+            # display cart and bill
+            display_cart_and_bill()
+
+        elif ch == 4:
+            # exit
+            break
 
 
 while True:
@@ -147,20 +180,13 @@ while True:
         change_fruit()
 
     elif ch == 5:
-        # add to cart
-        add_to_cart()
-
+        # display all fruits
+        display_fruit()
     elif ch == 6:
-        # display
-        diplay_fruit()
-    elif ch == 7:
-        # display cart
-        display_cart()
+        # manage cart
+        manage_cart()
 
-    elif ch == 8:
-        # delete item from cart
-        delete_from_cart()
-    elif ch == 9:
+    elif ch == 7:
         break
     else:
         print('Invalid choice')
